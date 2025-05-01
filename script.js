21a85378256fa1372ad46b45c356427a6b0f37c1
diggajbaral <atdiggaj@gmail.com>
2025-05-01 11:33:55 +0545

    learningJSthis

--- script.js
+++ script.js
@@ -1,21 +1,9 @@
 
-function nonrepeat(str) {
-
-    const charCount = {};
-
-    for (let char of str) {
-        charCount[char] = (charCount[char] || 0) + 1;
+const student = {
+    name : "Sita",
+    sayHello : function() {
+        console.log("Hello, I am " + this.name);
     }
+};
 
-    for (let char of str) {
-        if (charCount[char] === 1) {
-            return char;
-        }
-    }
-
-    return null;
-}
-
-console.log(nonrepeat('stress'));
-console.log(nonrepeat('rama'));
-console.log(nonrepeat('ram'));
+personalbar.sayHello();
