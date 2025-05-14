df1eef4a2fb172ea8265cc67d9b1e8f55e2743e9
diggajbaral <atdiggaj@gmail.com>
2025-05-14 09:56:56 +0545

    continue

---
+++ JS100/04 Recipe Book/script.js
@@ -0,0 +1,10 @@
+const apiKey = "275d58779ccf4e22af03e792e8819fff";
+const recipeList = document.querySelector("recipe-list")
+
+function displayRecipes(recipes) {
+    recipeList.innerHTML = "";
+    recipes.forEach((recipe) => {
+        const recipeItem = document.createElement("li");
+        
+    })
+}
