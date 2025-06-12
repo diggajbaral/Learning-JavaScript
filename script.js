20ee4fded64c0529e5116d4a1cfeaea2fb99924e
diggajbaral <atdiggaj@gmail.com>
2025-06-12 11:39:11 +0545

    for of loop

--- script.js
+++ script.js
@@ -1,20 +1,11 @@
 
-let index = 0
-while (index <= 10) {
-    // console.log('Index at:', index)
-    index = index + 2
+const greeting = "hello word"
+
+for (const greet of greeting) {
+
+    if (greet === ' ') {
+        continue
+    }
+    console.log(`Each char is, ${greet}`);
+
 }
-
-
-let chillArray = ['sita', 'ram', 'hanuman']
-let arr = 0;
-while (arr < chillArray.length) {
-    // console.log(chillArray[arr])
-    arr++;
-}
-
-let score = 11
-do {
-    // console.log(`Score is ${score}`);
-    score++
-} while (score <= 10)
