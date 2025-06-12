173a64b855a9d157647e2cf2f3e6231279026080
diggajbaral <atdiggaj@gmail.com>
2025-06-12 11:30:54 +0545

    for, while and do loop

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 
-( function chai() {
-    console.log('Connected');
-} ) ()
+let index = 0
+while (index <= 10) {
+    // console.log('Index at:', index)
+    index = index + 2
+}
+
+
+let chillArray = ['sita', 'ram', 'hanuman']
+let arr = 0;
+while (arr < chillArray.length) {
+    // console.log(chillArray[arr])
+    arr++;
+}
+
+let score = 11
+do {
+    // console.log(`Score is ${score}`);
+    score++
+} while (score <= 10)
